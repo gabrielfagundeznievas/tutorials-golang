e400478e3feb7d97b54ee72f3278ea0857842a9d
gabrielfaguendeznievas <gabrielfaguendeznievas@gmail.com>
2024-06-07 17:26:53 -0300

    feat: add Hellos function

--- hello_two/hello.go
+++ hello_two/hello.go
@@ -8,12 +8,14 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including the log entry prefix and a flag to disable printing the time, source file, and line number.
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
 	// Request a greeting message.
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Gladys")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
